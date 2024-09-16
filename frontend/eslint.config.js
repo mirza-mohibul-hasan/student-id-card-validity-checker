f0887a5e08995ece1a1d54d2517e87fbf1ddab61
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2024-09-16 22:29:16 +0600

    Frontend Modified to handle two model and added spinner

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,5 @@
 import js from "@eslint/js";
-import globals from "globals";
+import globals, { node } from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -10,6 +10,7 @@ export default [
     files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
+      node: 2024,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: "latest",
--- frontend/src/components/UploadForm.jsx
+++ frontend/src/components/UploadForm.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import axios from "axios";
-
+import ClockLoader from "react-spinners/ClockLoader";
 const UploadForm = () => {
   const [name, setName] = useState("");
   const [university, setUniversity] = useState("");
   const [image, setImage] = useState(null);
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
-
+  const [model, setModel] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
   const handleImageChange = (e) => {
     setImage(e.target.files[0]);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setResponse(null);
     if (!name || !university || !image) {
       setError("Please fill out all fields.");
       return;
@@ -26,8 +27,11 @@ const UploadForm = () => {
     formData.append("image", image);
 
     try {
+      setIsLoading(true);
       const res = await axios.post(
-        "http://localhost:5000/process-image",
+        model === 1
+          ? "http://localhost:5000/process-image-yolo"
+          : "http://localhost:5000/process-image-nlp",
         formData,
         {
           headers: { "Content-Type": "multipart/form-data" },
@@ -39,11 +43,35 @@ const UploadForm = () => {
       setError("Error uploading data.");
       console.log(err);
       setResponse(null);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
+    <div className="max-w-md mx-auto my-4 p-6 bg-white rounded-lg shadow-md">
+      <p className="text-md py-2 text-center">
+        Please Choose a Model, Default is YOLO
+      </p>
+      <div className="flex w-full">
+        <button
+          onClick={() => setModel(1)}
+          className={`card rounded-box grid h-10 flex-grow place-items-center ${
+            model === 1 ? "bg-blue-500 text-white" : "bg-base-200"
+          }`}
+        >
+          YOLO Model
+        </button>
+        <div className="divider divider-horizontal">OR</div>
+        <button
+          onClick={() => setModel(2)}
+          className={`card rounded-box grid h-10 flex-grow place-items-center ${
+            model === 2 ? "bg-blue-500 text-white" : "bg-base-200"
+          }`}
+        >
+          NLP Model
+        </button>
+      </div>
       <h1 className="text-2xl font-bold mb-4 text-center">
         ID Card Validation
       </h1>
@@ -101,9 +129,20 @@ const UploadForm = () => {
         {error && <p className="text-red-500 mt-4">{error}</p>}
       </form>
 
+      {isLoading && (
+        <div className="flex flex-col items-center my-4">
+          <p className="text-center">Loading..... Please Wait🙂</p>
+          <div>
+            <ClockLoader />
+          </div>
+        </div>
+      )}
+
       {response && (
-        <div className="mt-8 p-4 bg-gray-100 rounded-md">
-          <h2 className="text-lg font-bold">Validation Results</h2>
+        <div className="mt-4 p-4 bg-gray-100 rounded-md">
+          <h2 className="text-lg font-bold text-center border-b-2">
+            Validation Results
+          </h2>
           <p>
             <strong>ID Card Valid:</strong>{" "}
             {response.is_valid_card ? "Yes" : "No"}
@@ -118,6 +157,21 @@ const UploadForm = () => {
             <strong>Expiration Status:</strong>{" "}
             {response.is_expired ? "Expired" : "Valid"}
           </p>
+
+          <div className="mt-4">
+            <h3 className="text-lg font-bold text-center border-b-2">
+              Test Results
+            </h3>
+            <p>
+              <strong>Detected Name:</strong> {response.fields.Name}
+            </p>
+            <p>
+              <strong>Detected University:</strong> {response.fields.University}
+            </p>
+            <p>
+              <strong>Detected Expiration:</strong> {response.fields.Expiration}
+            </p>
+          </div>
         </div>
       )}
     </div>
